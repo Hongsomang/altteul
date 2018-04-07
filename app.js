47d7f0734463df445a57a6b9bb2b78a56437a441
Hongsomang <ghdthakd12@naver.com>
2018-04-07 17:17:43 +0900

    captain 회원가입

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ app.set('port',process.env.PORT||2018);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var signup_module=require('./module/worker/account/signup');
-var signin_module=require('./module/worker/account/signin');
-//회원가입
+var worker_signup_module=require('./module/worker/account/signup');
+var worker_signin_module=require('./module/worker/account/signin');
+var captain_signup_module=require('./module/captain/account/signup');
+//worker회원가입
 app.post('/account/worker/signup',function (req,res) {
     var worker_phonenumber=req.body.worker_phonenumber;
     var first_name=req.body.first_name;
@@ -16,7 +17,7 @@ app.post('/account/worker/signup',function (req,res) {
     var name=first_name+last_name;
     var password=req.body.password;
     console.log("회원가입:"+worker_phonenumber+" "+name+" "+password);
-    signup_module.signin(worker_phonenumber,name,password,function (err,result) {
+    worker_signup_module.signup(worker_phonenumber,name,password,function (err,result) {
         if(err){
             console.log(err);
             res.send(400);
@@ -30,12 +31,12 @@ app.post('/account/worker/signup',function (req,res) {
         }
     });
 });
-//로그인
+//worker로그인
 app.post('/account/worker/signin',function (req,res) {
     var worker_phonenumber=req.body.worker_phonenumber;
     var password=req.body.password;
     console.log("로그인:"+worker_phonenumber+" "+password);
-    signin_module.signin(worker_phonenumber,password,function (err,result,row) {
+    worker_signin_module.signin(worker_phonenumber,password,function (err,result,row) {
         console.log('result:'+result);
         if(err){
             console.log(err);
@@ -50,6 +51,29 @@ app.post('/account/worker/signin',function (req,res) {
         }
     });
 
+});
+//captain 회원가입
+app.post('/account/captain/signin',function (req,res) {
+    var captain_phonenumber=req.body.captain_phonenumber;
+    var first_name=req.body.first_name;
+    var last_name=req.body.last_name;
+    var name=first_name+last_name;
+    var password=req.body.password;
+    console.log("회원가입:"+captain_phonenumber+" "+name+" "+password);
+    captain_signup_module.signup(captain_phonenumber,name,password,function (err,result) {
+        if(err){
+            console.log(err);
+            res.send(400);
+        }
+        if(result=='good'){
+            res.status(201).end('성공');
+        }
+        else if(result==null){
+            res.status(404).end('실패');
+            
+            console.log("회원가입 실패")
+        }
+    });
 });
 
 
--- module/captain/account/signup.js
+++ module/captain/account/signup.js
@@ -3,9 +3,9 @@ var mysql=require('mysql');
 var sql=require('../.././db.js');
 var conn=mysql.createConnection(sql);
 conn.connect();
-exports.signup=function (work_phonenumber,name,password,callback) {
+exports.signup=function (captain_phonenumber,name,password,callback) {
     console.log("signup start");
-    conn.query('insert into worker(worker_phonenumber,name,password) values (?,?,?)',[work_phonenumber,name,password],function (err,row) {
+    conn.query('insert into captain(captain_phonenumber,name,password) values (?,?,?)',[captain_phonenumber,name,password],function (err,row) {
         if(err){
             console.log(err);
             callback(err,null);
